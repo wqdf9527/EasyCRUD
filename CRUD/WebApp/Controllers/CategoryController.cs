7f4cdddb1b24751e333889fb5b4ee81153d40a61
unknown <wqdf9527@gmail.com>
2019-01-16 00:48:12 +0800

    第一次更新
    第一次更新簡易CRUD

---
+++ CRUD/WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult Insert()
+        {
+            if (Request.Form.Count > 0)
+            {
+                //接收表單傳過來的資料
+                Categories _category = new Categories();
+                _category.CategoryName = Request.Form["CategoryName"];
+                _category.Description = Request.Form["Description"];
+
+                db.Categories.Add(_category);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id=1)
+        {
+            return View(db.Categories.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            //接收表單傳過來得資料
+            Categories _category = db.Categories.Find(Convert.ToInt32(Request.Form["CategoryID"]));
+            _category.CategoryName = Request.Form["CategoryName"];
+            _category.Description = Request.Form["Description"];
+
+            db.Entry(_category).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id = 0)
+        {
+           
+            Categories _category = db.Categories.Find(id);
+            db.Categories.Remove(_category);
+            db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CRUD/WebApp/Models/CategoryDataContext.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WebApp.Models
+{
+    public class CategoryDataContext
+    {
+        static string strConn = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+
+        //讀取所有產品分類資料
+        public static List<Category> LoadCategories()
+        {
+            List<Category> categories = new List<Category>();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "select CategoryID,CategoryName,Description from Categories";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Category _category = new Category();
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                        categories.Add(_category);
+                    }
+                    dr.Close();
+                }
+            }
+            return categories;
+        }
+
+        //新增產品分類資料
+        public static void InsertCategory(Category category)
+        {
+            string strCmd = "insert Categories(CategoryName,Description)values(@CategoryName,@Description)";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        //根據分類編號讀取要修改的產品分類資料
+        public static Category LoadCategoryByID(int CategoryID)
+        {
+            Category _category = new Category();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string strCmd = "select CategoryID,CategoryName,Description from Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                    }
+                    dr.Close();                   
+                }
+            }
+            return _category;
+        }
+
+
+        //修改產品分類資料
+        public static void EditCategory(Category category)
+        {
+           string strCmd = "update Categories set CategoryName=@CategoryName,Description=@Description where CategoryID=@CategoryID";
+          using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();                    
+                }
+            }
+        }
+
+        //刪除分類資料
+        public static void DeleteCategory(int CategoryID)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "Delete Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
